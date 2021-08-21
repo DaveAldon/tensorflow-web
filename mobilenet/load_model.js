c29b47f7d34611ac53189ddc7ac1fbb2d4e76e0a
DaveAldon <crawford@mail.gvsu.edu>
2021-08-21 15:53:02 -0400

    Updated models

--- mobilenet/load_model.js
+++ mobilenet/load_model.js
@@ -5,27 +5,39 @@ import * as tf from '@tensorflow/tfjs';
 export const load_model = async () => {
     const model = await tf.loadGraphModel('https://raw.githubusercontent.com/DaveAldon/tensorflow-web/master/output_converter_model/model.json');
 
-    const sample_text = ['The movie was cool. The animation and the graphics ','were out of this world. I would recommend this movie.', "sdfsd"]
     const sequence_length = 100
-    const seed = "hello"
+    let seed = "hello"
+    let s = seed
     const n_chars = 10;
     let generated = "";
 
-    const p = await model.executeAsync(
+    /* const p = await model.executeAsync(
         { 'lstm_input' : tf.zeros([1, 100, 10]) },
         [ 'dense'])
-        tf.print(p)
+        tf.print(p) */
 
     for (let i in [...Array(n_chars).keys()]) {
-        const X = tf.zeros([1, 100, 10])
-        console.log(X)
+         const X = tf.zeros([1, 100, 10], 'float32')
         for (const [index, element] of seed.split('').entries()) {
             X[0, (sequence_length - seed.length) + index, element.charCodeAt(0)] = 1;
-        }
-        const predicted = model.predict(X);
-        //console.log(predicted)
+        } 
+        //const predicted = await model.executeAsync(tf.zeros([1, 100, 10]))
+        //const predicted = model.predict(tf.zeros([1, 100, 10]));
+        const predicted = await model.executeAsync(X);
+
+        console.log("OUT:",predicted[1])
+        /* const predicted = await model.executeAsync(
+        { 'lstm_input' : tf.zeros([1, 100, 10]) },
+        [ 'dense']) */
+
+        //const next_index = tf.argMax(predicted)
+        const next_char = String.fromCharCode(predicted)
+        //console.log("next:",predicted)
+        generated += next_char
+        seed = seed.slice(1) + next_char
     }
 
+    console.log("generated:",generated, s)
 
 
     /* const inputData = tf.tensor(sample_text, [1, 3]);
